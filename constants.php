0a2d50e75761279338edce2e9079783f23ff9640
Eduard <eduard@keemail.me>
2018-10-03 20:02:38 +0200

    Remove

--- constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DEBUG', true);
-define('DIRECTORY', '/tmp/');
-
-//define('KB', 1024);
-define('MB', 1048576);
-define('GB', 1073741824);
-//define('TB', 1099511627776);
-define('MAXFILESIZE', 1*GB);
